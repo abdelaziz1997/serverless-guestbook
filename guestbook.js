4e4c9581ad37e85e722707f445b16d698e1861a1
abdelaziz1997 <54324666+abdelaziz1997@users.noreply.github.com>
2021-08-15 02:00:14 +0100

    Updating API URL
    Updating API URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://204556fc.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
